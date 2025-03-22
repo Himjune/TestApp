ed416fa9433233c2ae509c37505c85aa1f4ccfda
prepod <some@mail.ru>
2025-03-22 16:44:51 +0300

    Finished text

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -47,7 +47,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello H",
+        text = "Hello Hello",
         modifier = modifier
     )
 }
